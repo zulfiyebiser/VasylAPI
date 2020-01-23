3b2f309888b503629b4b2b08730cfc135bfaba8c
cybertekschool <github@cybertekschool.com>
2020-01-23 15:28:37 -0500

    exchange rates test

--- src/test/java/com/automation/tests/day2/ExchangeRatesAPITests.java
+++ src/test/java/com/automation/tests/day2/ExchangeRatesAPITests.java
@@ -3,6 +3,10 @@ package com.automation.tests.day2;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,17 +14,17 @@ public class ExchangeRatesAPITests {
     private String baseURI = "https://api.openrates.io/";
 
     @Test
-    public void test1(){
+    public void test1() {
         Response response = given().
-                get(baseURI+"latest");
+                get(baseURI + "latest");
         //verify status code
         assertEquals(200, response.getStatusCode());
         System.out.println(response.prettyPrint());
     }
 
     @Test
-    public void test2(){
-        Response response = given().get(baseURI+"latest");
+    public void test2() {
+        Response response = given().get(baseURI + "latest");
         //verify that content type is json
         assertEquals(200, response.getStatusCode());
         //verify that data is coming as json
@@ -32,10 +36,30 @@ public class ExchangeRatesAPITests {
     //base it's a query parameter that will ask web service to change currency from eu to something else
 
     @Test
-    public void test3(){
+    public void test3() {
         //#TASK: get currency exchange rate for dollar. By default it's euro.
-        Response response = given().get(baseURI+"latest?base=USD");
+//        Response response = given().get(baseURI+"latest?base=USD");
+        Response response = given().
+                baseUri(baseURI + "latest").
+                queryParam("base", "USD").
+                get();
         assertEquals(200, response.getStatusCode());
         System.out.println(response.prettyPrint());
+    }
+
+    //    #TASK: verify that response body, for latest currency rates, contains today's date (2020-01-23 | yyyy-MM-dd)
+    @Test
+    public void test4() {
+        Response response = given().
+                baseUri(baseURI + "latest").
+                queryParam("base", "GBP").
+                get();
+
+        String todaysDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        System.out.println("Today's date: " + todaysDate);
+
+        assertEquals(200, response.getStatusCode());
+        assertTrue(response.getBody().asString().contains(todaysDate));
     }
 }
