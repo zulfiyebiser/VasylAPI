8d5d6c6149de2d0e7e5c9c30e2ba12a74f3b9bbf
cybertekschool <github@cybertekschool.com>
2020-01-23 11:40:10 -0500

    created ORDSTests class

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,4 +1,14 @@
 package com.automation.tests.day2;
 
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.* ;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ORDSTests {
+
+    @Test
+    public void test1(){
+
+    }
 }
