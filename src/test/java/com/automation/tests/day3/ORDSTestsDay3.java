a01a32ec38723c5bd01b351b0b47966859ea95f3
cybertekschool <github@cybertekschool.com>
2020-01-24 11:39:19 -0500

    created ORDSTestsDay3 class

--- src/test/java/com/automation/tests/day3/ORDSTestsDay3.java
+++ src/test/java/com/automation/tests/day3/ORDSTestsDay3.java
@@ -22,13 +22,29 @@ public class ORDSTestsDay3 {
     //we are asking for json as a response
     @Test
     public void test1() {
-         given().
+        given().
                 accept("application/json").
                 get("/employees").
-         then().
+                then().
                 assertThat().statusCode(200).
                 and().assertThat().contentType("application/json").
                 log().all(true);
+    }
+
+    //path parameter - to point on specific resource /employee/:id/ - id it's a path parameter
+    //query parameter - to filter results, or describe new resource :
+    // POST /users?name=James&age=60&job-title=SDET
+    //or to filter GET /employee?name=Jamal get all employees with name Jamal
+    @Test
+    public void test2() {
+        given().
+                accept("application/json").
+                pathParam("id", 100).
+        when().get("/employees/{id}").
+        then().assertThat().statusCode(200).
+        and().assertThat().body("employee_id", is(100)).log().all(true);
+        //body ("phone_number") --> 515.123.4567
+        //is is coming from ---> import static org.hamcrest.Matchers.*;
     }
 
 
