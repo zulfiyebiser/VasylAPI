658daa4c9bfa107a4247af535c56ec3455137ef4
cybertekschool <github@cybertekschool.com>
2020-01-27 16:49:36 -0500

    day 4

--- src/test/java/com/automation/tests/day4/MetaWeatherJsonPathTests.java
+++ src/test/java/com/automation/tests/day4/MetaWeatherJsonPathTests.java
@@ -27,6 +27,7 @@ public class MetaWeatherJsonPathTests {
     public static void setup() {
         baseURI = ConfigurationReader.getProperty("meta.weather.uri");
     }
+
     /**
      * TASK
      * Given accept type is JSON
@@ -41,28 +42,97 @@ public class MetaWeatherJsonPathTests {
         given().
                 accept(ContentType.JSON).
                 queryParam("query", "New").
-        when().
+                when().
                 get("/search").
-        then().
+                then().
                 assertThat().
-                    statusCode(200).
-                    body("", hasSize(5)).
-                    log().body(true);
+                statusCode(200).
+                body("", hasSize(5)).
+                log().body(true);
 
     }
+
+
+    /**
+     * TASK
+     * Given accept type is JSON
+     * When users sends a GET request to "/search"
+     * And query parameter is New
+     * Then user verifies that 1st object has following info:
+     * |title   |location_type|woeid  |latt_long          |
+     * |New York|City         |2459115|40.71455,-74.007118|
+     */
+    @Test
+    @DisplayName("Verifies that 1st city has following info:New York, City, 2459115, 40.71455,-74.007118")
+    public void test2() {
+            given().
+                    accept(ContentType.JSON).
+                    queryParam("query", "New").
+            when().
+                    get("/search").
+            then().
+                    assertThat().
+                        statusCode(200).
+                        body("title[0]", is("New York")).
+                        body("location_type[0]", is("City")).
+                        body("woeid[0]", is(2459115)).
+                        body("latt_long[0]", is("40.71455,-74.007118")).
+                        log().body(true);
+    }
+
+    @Test
+    @DisplayName("Verifies that 1st city has following info:New York, City, 2459115, 40.71455,-74.007118")
+    public void test2_2() {
+        Map<String,String> expected = new HashMap<>();
+        expected.put("title", "New York");
+        expected.put("location_type", "City");
+        expected.put("woeid", "2459115");
+        expected.put("latt_long", "40.71455,-74.007118");
+
+        Response response = given().
+                                accept(ContentType.JSON).
+                                queryParam("query", "New").
+                            when().
+                                get("/search");
+        JsonPath jsonPath = response.jsonPath();
+        //String.class, String.class will force jsonpath to return map with String as key and value
+        assertEquals(expected, jsonPath.getMap("[0]", String.class, String.class));
+        //for first title, title[0], but for first object, we can say just [0]
+    }
+
+
 }
-
-
-
-
 /**
+ * <p>
  * TASK
  * Given accept type is JSON
  * When users sends a GET request to "/search"
- * And query parameter is New
- * Then user verifies that 1st object has following info:
- * |title   |location_type|woeid  |latt_long          |
- * |New York|City         |2459115|40.71455,-74.007118|
+ * And query parameter is 'Las'
+ * Then user verifies that payload  contains following titles:
+ * |Glasgow  |
+ * |Dallas   |
+ * |Las Vegas|
+ * <p>
+ * <p>
+ * TASK
+ * Given accept type is JSON
+ * When users sends a GET request to "/search"
+ * And query parameter is 'Las'
+ * Then verify that every item in payload has location_type City
+ * <p>
+ * <p>
+ * TASK
+ * Given accept type is JSON
+ * When users sends a GET request to "/location"
+ * And path parameter is '44418'
+ * Then verify following that payload contains weather forecast sources
+ * |BBC                 |
+ * |Forecast.io         |
+ * |HAMweather          |
+ * |Met Office          |
+ * |OpenWeatherMap      |
+ * |Weather Underground |
+ * |World Weather Online|
  * <p>
  * TASK
  * Given accept type is JSON
