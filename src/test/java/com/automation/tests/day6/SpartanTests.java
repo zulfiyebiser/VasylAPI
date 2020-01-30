668e902c1a75b8c6aa518e036dc1702526363f8e
cybertekschool <github@cybertekschool.com>
2020-01-30 15:59:34 -0500

    day 6

--- src/test/java/com/automation/tests/day6/SpartanTests.java
+++ src/test/java/com/automation/tests/day6/SpartanTests.java
@@ -29,7 +29,7 @@ import static org.hamcrest.Matchers.*;
 public class SpartanTests {
 
     @BeforeAll
-    public static void setup(){
+    public static void setup() {
         baseURI = ConfigurationReader.getProperty("spartan.uri");
     }
 
@@ -42,7 +42,17 @@ public class SpartanTests {
 
     @Test
     @DisplayName("Verify that /spartans end-point returns 200 and content type as JSON")
-    public void test1(){
+    public void test1() {
+        //web service may return different content type
+        //and to request JSON, you can just put in the given part  ContentType.JSON
+        //If you want to ask for XML, you can put ContentType.XML
+        //but, if web service configured only for JSON
+        //it will not give you anything else
+        //GET, PUT, POST, DELETE, etc. - HTTP verbs or methods
+        //GET - to get the data from web service
+        //PUT - update existing record
+        //DELETE - delete something, like delete spartan
+        //PATCH - partial update of existing record
         given().
                 accept(ContentType.JSON).
         when().
@@ -54,15 +64,16 @@ public class SpartanTests {
     }
 
 
-    /** TASK
+    /**
+     * TASK
      * given accept content type as XML
      * when user sends GET request to /spartans
      * then user verifies that status code is 200
      * and user verifies that content type is XML
      */
-    @Test//comeback at 12:12
+    @Test
     @DisplayName("Verify that /spartans end-point returns 200 and content type as XML")
-    public void test2(){
+    public void test2() {
         //asking
         //accept(ContentType.XML). <- you are asking for payload format as XML from web service
         //receiving
@@ -77,27 +88,57 @@ public class SpartanTests {
 
     }
 
-    /** TASK
+    /**
+     * TASK
      * given accept content type as JSON
      * when user sends GET request to /spartans
      * then user saves payload into collection
+     *
+     * ###################
+     * We can convert payload (JSON body for example) into collection.
+     * if it's a single variable: "name" : "James", we acn store in String or List<String>
+     * If, there multiple names in the payload, we cannot use single String as a storage
+     * instead, use List<String>
+     * If payload returns object:
+     *  {
+     *  "name": "James"
+     *  "age" 25
+     *  }
+     *  Then, we can store this object (on our, java side, as POJO or Map<String, ?>)
+     *  If it's a POJO, we need to create corresponding POJO class, in order to map properties
+     *  from json and java object:
+     *          Java class         JSON file
+     *      private String name |  "name"
+     *      private int age     |  "age"
+     *
+     *      If you want to use different variable name in Java class, use @SerializedName annotation
+     *
+     *          Java class            JSON file
+     *      @SerializedName("name")
+     *      private String firstName |  "name"
+     *      private int age          |  "age"
+     *
+     *      otherwise,  Gson, jackson, or any other Json parser, will not be able to map properties correctly.
+     *      Serialization - from POJO (java object) to stream of bytes, let's say to JSON
+     *      Deserialization - from stream of bytes, let's say JSON into POJO (java object)
      */
     @Test
     @DisplayName("Save payload into java collection")
-    public void test3(){
+    public void test3() {
         Response response = given().
-                                contentType(ContentType.JSON).
-                            when().
-                               get("/spartans");
+                contentType(ContentType.JSON).
+                when().
+                get("/spartans");
 
         List<Map<String, ?>> collection = response.jsonPath().get();
 
-        for(Map<String, ?> map: collection){
+        for (Map<String, ?> map : collection) {
             System.out.println(map);
         }
     }
 
-    /** TASK
+    /**
+     * TASK
      * given accept content type as JSON
      * when user sends GET request to /spartans
      * then user saves payload into collection of Spartan
@@ -105,34 +146,35 @@ public class SpartanTests {
 
     @Test
     @DisplayName("Save payload into java collection of Spartan")
-    public void test4(){
+    public void test4() {
         Response response = given().
-                                contentType(ContentType.JSON).
-                            when().
-                                get("/spartans").prettyPeek();
+                contentType(ContentType.JSON).
+                when().
+                get("/spartans").prettyPeek();
         //whenever you see: Class object = response.jsonPath.getObject() | deserialization
         List<Spartan> collection = response.jsonPath().getList("", Spartan.class);
 
-        for(Spartan spartan: collection){
+        for (Spartan spartan : collection) {
             System.out.println(spartan);
         }
     }
 
-    /** TASK
+    /**
+     * TASK
      * given accept content type as JSON
      * when user sends POST request to /spartans
      * and user should be able to create new spartan
-     *      |gender|name           |phone     |
-     *      | male |Mister Twister |5712134235|
+     * |gender|name           |phone     |
+     * | male |Mister Twister |5712134235|
      * then user verifies that status code is 201
-     *
+     * <p>
      * 201 - means created. Whenever you POST something, you should get back 201 status code
      * in case of created record
      */
 
     @Test
     @DisplayName("Create new spartan and verify that status code is 201")
-    public void test5(){
+    public void test5() {
         //builder pattern, one of the design patterns in OOP
         //instead of having too many different constructors
         //we can use builder pattern and chain with{preopertyName} methods to specify properties of an object
@@ -147,10 +189,10 @@ public class SpartanTests {
         spartan.setPhone(5712134235L); //at least 10 digits
 
         Response response = given().
-                                contentType(ContentType.JSON).
-                                body(spartan).
-                            when().
-                                post("/spartans");
+                contentType(ContentType.JSON).
+                body(spartan).
+                when().
+                post("/spartans");
         assertEquals(201, response.getStatusCode(), "Status code is wrong!");
         assertEquals("application/json", response.getContentType(), "Content type is invalid!");
         assertEquals(response.jsonPath().getString("success"), "A Spartan is Born!");
@@ -159,7 +201,7 @@ public class SpartanTests {
 
         Spartan spartan_from_response = response.jsonPath().getObject("data", Spartan.class);
 
-        System.out.println("Spartan id: "+spartan_from_response.getSpartanId());
+        System.out.println("Spartan id: " + spartan_from_response.getSpartanId());
 
 //        //delete spartan that you've just created
 //        when().delete("/spartans/{id}", spartan_from_response.getSpartanId()).
@@ -170,7 +212,7 @@ public class SpartanTests {
 
     @Test
     @DisplayName("Delete user")
-    public void test6(){
+    public void test6() {
         int idOfTheUserThatYouWantToDelete = 125;
 
         Response response = when().delete("/spartans/{id}", idOfTheUserThatYouWantToDelete);
@@ -180,22 +222,22 @@ public class SpartanTests {
 
     @Test
     @DisplayName("Delete half of the records")
-    public void test7(){
+    public void test7() {
         int idOfTheUserThatYouWantToDelete = 125;
         Response response = given().
-                                accept(ContentType.JSON).
-                            when().
-                                get("/spartans");
+                accept(ContentType.JSON).
+                when().
+                get("/spartans");
         //I collected all user id's
         List<Integer> userIDs = response.jsonPath().getList("id");
 
         //I sorted user id's in descending order
         Collections.sort(userIDs, Collections.reverseOrder());
-        System.out.println("Before: "+userIDs);
+        System.out.println("Before: " + userIDs);
 
         //I went through half of the collection, and deleted half of the users
         //userIDs.size()/2 - represents half of the spartans
-        for(int i=0; i< userIDs.size()/2;i++){
+        for (int i = 0; i < userIDs.size() / 2; i++) {
             //will delete spartan based on id that you specify
             when().delete("/spartans/{id}", userIDs.get(i));
         }
@@ -205,43 +247,49 @@ public class SpartanTests {
 //        response.prettyPeek();
     }
 
-//    "Phone number should be at least 10 digit and UNIQUE!!",
+    //    "Phone number should be at least 10 digit and UNIQUE!!",
     @Test
-    @DisplayName("Add 100 test users to Spartan app")
-    public void test8(){
+    @DisplayName("Add 10 test users to Spartan app")
+    public void test8() {
         Faker faker = new Faker();
-        for(int i=0; i<100; i++){
+        for (int i = 0; i < 10; i++) {
             Spartan spartan = new Spartan();
 
             spartan.setName(faker.name().firstName());
             //remove all non-digits
-            //
-            String phone = faker.phoneNumber().subscriberNumber(12).replaceAll("\\D", "");
+            // replaceAll() < takes regex (regular expression)
+            // regex - it's a pattern, means that 1 character can represent group of chars/symbols/digits
+            // \\D - everything that is not a digit (0-9)
+
+            String phone = faker.phoneNumber().subscriberNumber(11).replaceAll("\\D", "");
+//           phone.matches("\\d"); check if this string contains only digits
+//           phone.matches("[a-x]"); check if this string contains letters in the range from a to x
             //convert from String to Long
             spartan.setPhone(Long.parseLong(phone));
 
-            spartan.setGender("Male");
+            spartan.setGender("Female");
 
             System.out.println(spartan);
 
             Response response = given().
-                    contentType(ContentType.JSON).
-                    body(spartan).
-                    when().
-                    post("/spartans");
-
+                                    contentType(ContentType.JSON).
+                                    body(spartan).
+                                when().
+                                    post("/spartans").prettyPeek();
+            
+             //whenever you successfully add new spartan, you will get this message: "A Spartan is Born!",
             System.out.println(response.jsonPath().getString("success"));
-
+            //verify that response status code is 201,
+            //in our case 201 means that post request went well
             assertEquals(201, response.getStatusCode());
-              //come back at 3:20
+
         }
     }
 
 
-
     @Test
     @DisplayName("Get all spartan id's and print it as list")
-    public void test9(){
+    public void test9() {
 
     }
 
