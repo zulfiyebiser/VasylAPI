15bbfebece55965668542876e67b6d2012be4a16
cybertekschool <github@cybertekschool.com>
2020-01-30 11:41:38 -0500

    day 6

--- src/test/java/com/automation/pojos/Spartan.java
+++ src/test/java/com/automation/pojos/Spartan.java
@@ -1,4 +1,70 @@
 package com.automation.pojos;
 
+import com.google.gson.annotations.SerializedName;
+
+//          {
+//           "id": 112,
+//           "name": "Vasya",
+//           "gender": "Male",
+//           "phone": 7654321876
+//           },
 public class Spartan {
+    @SerializedName("id")
+    private int spartanId;
+    private String name;
+    private String gender;
+    private long phone;
+
+    public Spartan(){
+
+    }
+
+    public Spartan(int spartanId, String name, String gender, long phone) {
+        this.spartanId = spartanId;
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public int getSpartanId() {
+        return spartanId;
+    }
+
+    public void setSpartanId(int spartanId) {
+        this.spartanId = spartanId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Spartan{" +
+                "spartanId=" + spartanId +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
 }
