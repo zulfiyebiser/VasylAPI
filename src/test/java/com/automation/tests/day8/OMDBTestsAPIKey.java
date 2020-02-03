9915ae7a1c943a199d58fe54f650145f1e9059a4
cybertekschool <github@cybertekschool.com>
2020-02-03 12:45:23 -0500

    day 8

--- src/test/java/com/automation/tests/day8/OMDBTestsAPIKey.java
+++ src/test/java/com/automation/tests/day8/OMDBTestsAPIKey.java
@@ -30,4 +30,17 @@ public class OMDBTestsAPIKey {
     public static void setup() {
         baseURI = ConfigurationReader.getProperty("omdb.uri");
     }
+
+    @Test
+    @DisplayName("Verify that unauthorized user cannot get info about movies from OMDB api")
+    public void test1(){
+        given().
+                contentType(ContentType.JSON).
+                queryParam("t", "Home Alone").
+        when().
+                get().prettyPeek().
+        then().assertThat().statusCode(401).body("Error", is("No API key provided."));
+        //401 Unauthorized - you are not allowed to access this web service
+    }
+
 }
