a980fc1f1afc9977ae567ff3506cc4dedbe4576b
cybertekschool <github@cybertekschool.com>
2020-02-04 14:56:56 -0500

    day 8

--- src/test/java/com/automation/tests/day9/BookITTests.java
+++ src/test/java/com/automation/tests/day9/BookITTests.java
@@ -103,8 +103,115 @@ public class BookITTests {
                 get("/api/rooms").prettyPeek();
         List<Room> rooms = response.jsonPath().getList("", Room.class);
 
-        for(Room room: rooms){
-            System.out.println(room);
+        for (Room room : rooms) {
+            System.out.println(room.getName());
         }
     }
+
+
+    @Test
+    @DisplayName("Create a new student")
+    public void test5() {
+
+    }
+
+    @Test
+    @DisplayName("Verify that B12 exists")
+    public void test5_1() {
+        ///api/batches/{batch-number}
+        given().
+                auth().oauth2(APIUtilities.getTokenForBookit()).
+        when().
+                get("/api/batches/{batch-number}", 12).
+        then().assertThat().statusCode(200).log().body(true);
+
+    }
+//    campus-location	required	name of the campus which team will be added to
+//    batch-number	required	number of the batch which team will be added to
+//    team-name	required	name of the team, should be uniq per campus
+
+    /**
+     * given valid token is provided for student team leader
+     * and user provides following query parameters
+     *  |campus-location|batch-number|team-name     |
+     *  |     VA        |    12      |Online_Hackers|
+     *  when user performs POST request to "/api/teams/team"
+     *  then user should verify that status code is 403
+     */
+    @Test
+    @DisplayName("Create a new team in B12 (negative)")
+    public void test6(){
+        //Online_Hackers
+//        POST /api/teams/team
+        given().
+                auth().oauth2(APIUtilities.getTokenForBookit()).
+                queryParam("campus-location", "VA").
+                queryParam("batch-number", 12).
+                queryParam("team-name", "Online_Hackers").
+        when().
+                post("/api/teams/team").
+       then().assertThat().statusCode(403).log().all(true);
+
+        //only teacher allowed to modify database. <---- authorization
+        // 403 Forbidden - that means you are not authorized to do this
+    }
+
+    /**
+     * given valid token is provided for teacher
+     * and user provides following query parameters
+     *  |campus-location|batch-number|team-name     |
+     *  |     VA        |    12      |Online_Hackers|
+     *  when user performs POST request to "/api/teams/team"
+     *  then user should verify that status code is 201
+     */
+
+    @Test
+    @DisplayName("Create a new team in B12 (positive)")
+    public void test7(){
+        //Online_Hackers
+//        POST /api/teams/team
+        given().
+                auth().oauth2(APIUtilities.getTokenForBookit("teacher")).
+                queryParam("campus-location", "VA").
+                queryParam("batch-number", 12).
+                queryParam("team-name", "Online_Hackers").
+                when().
+                post("/api/teams/team").
+                then().log().all(true);
+
+        //only teacher allowed to modify database. <---- authorization
+    }
+
+    //test: add yourself to the team
+    /**
+     *  {
+     *             "id": 5443,
+     *             "name": "Online_Hackers",
+     *             "members": [
+     *
+     *             ]
+     *         },
+     */
+    /**
+     * POST /api/students/student
+     *
+     * Query Parameters
+     * Parameter	    Demand	    Description
+     * first-name	    required	first name of the student
+     * last-name	    required	last name of the student
+     * email	        required	email of the student, will be used for an authentication
+     * password	        required	password of the account, will be used for an authentication
+     * role      	    required	role of the student, [student-team-leader, student-team-member]
+     * campus-location	required	name of the campus which student will be added to
+     * batch-number	    required	number of the batch which student will be added to
+     * team-name	    required	name of the team which student will be added to
+     *
+     * given valid token is provided for student team leader
+     * and user provides following query parameters
+     *  |first-name  |last-name    |email         |password    |role               |campus-location|batch-number|team-name      |
+     *  |    YourName|YourLastName |temp@email.com| anypassword|student-team-member|      VA       |    12      | Online_Hackers|
+     *  when user performs POST request to "/api/students/student"
+     *  then user should verify that status code is 403
+     */
+
 }
